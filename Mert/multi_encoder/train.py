35cc02d0ed1394837a8555774b8f84d5a1dfe8bb
QuadnucYard <2380433991@qq.com>
2022-08-20 17:31:57 +0800

    更新入口和训练配置文件

--- Mert/multi_encoder/train.py
+++ Mert/multi_encoder/train.py
@@ -1,71 +1,50 @@
-import os
-import sys
-from pathlib import Path
-
-from accelerate import Accelerator
-
-accelerator = Accelerator()
-accelerator.free_memory()
-
 import logging
-
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-sys.path.append(os.getcwd())
-
-from MNER.utils import getlogger
-
-logger = getlogger('Mert')
-if accelerator.is_main_process:
-    logger.info('Loading packages...')
-
-import warnings
+import os
 
 import torch
 import transformers
+from accelerate import Accelerator
 from torch.utils.tensorboard import SummaryWriter
 from transformers.trainer_utils import SchedulerType
 
-from datasets.flickr_dataset import getFlickrDataLoader
-from multi_encoder.config import MultiEncoderConfig
-from multi_encoder.model import MultiEncoder, MultiEncoderV2_2, MultiEncoderOutput
-from multi_encoder.train_config import MultiEncoderTrainConfig
-from multi_encoder.train_utils import evaluate, load_model_best, save_model, train
+from .model import MultiEncoderOutput
+from .train_config import MultiEncoderTrainConfig
+from .train_utils import evaluate, load_model_best, save_model, train
 
-warnings.filterwarnings("ignore")
-for log_name, log_obj in logging.Logger.manager.loggerDict.items():
-    if log_name != "Mert" and log_name != 'accelerate':
-        log_obj.disabled = True
 
-if __name__ == '__main__':
+def main_train(accelerator: Accelerator, logger: logging.Logger):
     writer = None
+
+    train_config = MultiEncoderTrainConfig()
+
     if accelerator.is_main_process:
         logger.info('Done.')
         logger.info('Loading model...')
 
-    multi_config = MultiEncoderConfig(d_text=64, d_vision=197)
-    encoder = MultiEncoderV2_2(config=multi_config)
-    model = MultiEncoderOutput(encoder)
+    model = MultiEncoderOutput(train_config.encoder)
     model_name = model.__class__.__name__
 
     if accelerator.is_main_process:
         writer = SummaryWriter(os.path.join("Mert/multi_encoder/tb_log", model_name))
     if accelerator.is_main_process:
-        #logger.info(f'Model:{model_name}|Trained_epoch:{int(model.trained_epoch.item())}')
         logger.info('Done.')
         logger.info('Constructing datasets...')
 
-    train_dl, val_dl = getFlickrDataLoader()
+    train_dl, val_dl = train_config.dataloader
 
     if accelerator.is_main_process:
         logger.info('Done.')
 
-    train_config = MultiEncoderTrainConfig()
     start_epoch = 0
     if train_config.load_ckpt:
-        ckpt = load_model_best(train_config, model)
-        if ckpt is not None:
-            start_epoch = ckpt["epoch"]
-        ckpt = None
+        try: # 这个不能起作用
+            ckpt = load_model_best(train_config, model)
+            if ckpt is not None:
+                start_epoch = ckpt["epoch"]
+            ckpt = None
+        except Exception as e:
+            logger.error("Fail to load checkpoint.\n" + e)
+
     logger.info(f"start-epoch: {start_epoch}")
     logger.info(f"device: {accelerator.device}")
 
@@ -77,7 +56,6 @@ if __name__ == '__main__':
         num_training_steps=train_config.epochs * len(train_dl),
     )
 
-    best_micro_f1 = -1.
     if accelerator.is_main_process:
         logger.info('Launching training.')
 
@@ -96,7 +74,7 @@ if __name__ == '__main__':
         if eval_loss < best_loss:
             best_loss = eval_loss
             if accelerator.is_main_process:
-                save_model(model, train_config.ckpt_name, epoch + 1, train_config, accelerator)
+                save_model(model, epoch + 1, train_config, accelerator)
         accelerator.print('-----------------------------------------------------------')
 
     if accelerator.is_main_process:
--- Mert/multi_encoder/train_config.py
+++ Mert/multi_encoder/train_config.py
@@ -1,4 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Tuple
+
+from datasets.DataLoaderX import DataLoaderX
+from datasets.flickr_dataset import getFlickrDataLoader
+from multi_encoder.config import MultiEncoderConfig
+from multi_encoder.model import MultiEncoder, MultiEncoderV2_2
 
 
 @dataclass
@@ -11,3 +17,8 @@ class MultiEncoderTrainConfig:
     save_state_interval = 100
     max_ckpt_num = 5
     load_ckpt = True
+
+    encoder: MultiEncoderV2_2 = field(
+        default_factory=lambda: MultiEncoderV2_2(config=MultiEncoderConfig(d_text=64, d_vision=197))
+    )
+    dataloader: Tuple[DataLoaderX, DataLoaderX] = field(default_factory=getFlickrDataLoader)
