9b3f928ce717c6954cf808dd2d315f8be08e20e9
QuadnucYard <2380433991@qq.com>
2022-08-30 19:26:30 +0800

    封装服务端

--- server/mert_server.py
+++ server/mert_server.py
@@ -3,11 +3,11 @@ import base64
 from io import BytesIO
 
 from flask import request, jsonify
-from PIL import Image
+from PIL import Image, ImageFile
 
 from server_utils import create_app, create_server
 
-from server.mert_service import MEL_step1, MEL_step2, MEL_step2V2
+from server.mert_service import MertService
 from server.wiki_service import query_entities
 
 parser = ArgumentParser()
@@ -16,29 +16,33 @@ parser.add_argument("-p", "--port", help="The port of server", type=int, default
 parser.add_argument("-w", "--wiki", help="Access Wikidata in this server", action="store_true")
 args = parser.parse_args()
 
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+Image.MAX_IMAGE_PIXELS = 4096**2
+
 
 def base64_pil(base64_str: str):
     image = base64.b64decode(base64_str.split("base64,")[1])
     image = BytesIO(image)
-    image = Image.open(image)
+    image = Image.open(image).convert("RGB")
     return image
 
 
 app = create_app(__name__)
+service = MertService(app.logger)
 
 
 @app.route("/mert/query", methods=["POST"])
 def query_controller():
     caption = request.json.get("caption", "")
-    image=request.json.get("image", None)
+    image = request.json.get("image", None)
     image = base64_pil(image) if image else Image.new("RGB", (32, 32), (0, 0, 0))
     app.logger.info(caption)
-    result = MEL_step1(caption, image)
+    result = service.MEL_step1(caption, image)
     app.logger.info(result)
     if args.wiki:
         query_result = query_entities(result["query"])
-        app.logger.info(query_result)
-        answer = MEL_step2V2(result["key"], query_result)
+        app.logger.info("Get query results")
+        answer = service.MEL_step2V2(result["key"], query_result)
         app.logger.info(answer)
         result = {"answer": answer, "wikidata": query_result}
 
@@ -49,7 +53,7 @@ def query_controller():
 def query_controller_back():
     key = request.json.get("key", type=int)
     query_results = request.json.get("data")
-    result = MEL_step2V2(key, query_results)
+    result = service.MEL_step2V2(key, query_results)
     return jsonify({"answer": result, "wikidata": query_results})
 
 
--- server/mert_service.py
+++ server/mert_service.py
@@ -1,3 +1,4 @@
+from logging import Logger
 import os
 from typing import Dict
 from PIL import Image
@@ -5,74 +6,75 @@ from PIL import Image
 os.environ['TRANSFORMERS_CACHE'] = 'F:/Multimodality-Link/.cache/'
 
 from transformers import logging as tlogging
+
 tlogging.set_verbosity_error()
 
-from transformers import BertModel, BertTokenizer, FlavaProcessor
+from transformers import BertModel, BertTokenizerFast, FlavaProcessor
 from Mert.EntityLinkPipeline import ELPreprocessOutput, EntityLinkPipeline_step1, EntityLinkPipeline_step2, EntityLinkPipelineV2
 from Mert.MNER.model import MertForNERwithESD_bert_only
 from Mert.multi_encoder.model import MultiEncoder
 from Mert.common_config import PretrainedModelConfig
 from Mert.Recall_entity.model import MertForEL
 
-print("Initialize models")
-NER_model = MertForNERwithESD_bert_only.from_pretrained(PretrainedModelConfig.nermodel_path).eval()
-print("NER_model ready")
-multi_model = MultiEncoder.from_pretrained(PretrainedModelConfig.multiencoder_path, forward_link=True).eval()
-print("multi_model ready")
-entity_model = BertModel.from_pretrained('bert-base-uncased').eval()
-print("entity_model ready")
-el_model = MertForEL.from_pretrained(PretrainedModelConfig.mertel_path).eval()
-print("el_model ready")
-flava_processor = FlavaProcessor.from_pretrained('facebook/flava-full')
-print("flava_processor ready")
-entity_processor = BertTokenizer.from_pretrained('bert-base-uncased')
-print("entity_processor ready")
-print("Service Init Complete")
 
-g_querykey = 0
-pipeline_cache: Dict[int, ELPreprocessOutput] = {}
+class MertService:
+    def __init__(self, logger: Logger) -> None:
+        self.logger = logger
+        logger.info("Initialize models")
+        self.NER_model = MertForNERwithESD_bert_only.from_pretrained(PretrainedModelConfig.nermodel_path).eval()
+        logger.info("NER_model ready")
+        self.multi_model = MultiEncoder.from_pretrained(PretrainedModelConfig.multiencoder_path).eval()
+        logger.info("multi_model ready")
+        self.entity_model = BertModel.from_pretrained('bert-base-uncased').eval()
+        logger.info("entity_model ready")
+        self.el_model = MertForEL.from_pretrained(PretrainedModelConfig.mertel_path).eval()
+        logger.info("el_model ready")
+        self.flava_processor = FlavaProcessor.from_pretrained('facebook/flava-full')
+        logger.info("flava_processor ready")
+        self.entity_processor = BertTokenizerFast.from_pretrained('bert-base-uncased')
+        logger.info("entity_processor ready")
+        logger.info("Service Init Complete")
 
+        self.g_querykey = 0
+        self.pipeline_cache: Dict[int, ELPreprocessOutput] = {}
 
-def MEL_step1(caption: str, image: Image):
-    '''First step: Obtain query entities from processor output.'''
-    global g_querykey
-    g_querykey += 1
-    output = EntityLinkPipeline_step1(
-        query_text=caption,
-        query_image=image,
-        multi_model=multi_model,
-        NER_model=NER_model,
-        multi_processor=flava_processor,
-    )
-    pipeline_cache[g_querykey] = output
-    query = [entity['entity'] for entity in output.entities]
-    return {"key": g_querykey, "query": query}
-
-
-def MEL_step2(key: int, queries: list):
-    result = EntityLinkPipeline_step2(
-        preoutput=pipeline_cache[key],
-        entity_model=entity_model,
-        entity_processor=entity_processor,
-        wikidata=queries,
-    )
-    del pipeline_cache[key]
-    return result
-
-
-def MEL_step2V2(key: int, query_results: list):
-    preoutput = pipeline_cache[key]
-    entities = preoutput.entities
-    del pipeline_cache[key]
-
-    for idx, q in enumerate(query_results):
-        ans = EntityLinkPipelineV2(
-            query_text=preoutput.query_text,
-            query_img=preoutput.query_image,
-            candidate_abs=[cand["abs"] for cand in q],
-            model=el_model,
-            processor=flava_processor,
+    def MEL_step1(self, caption: str, image: Image):
+        '''First step: Obtain query entities from processor output.'''
+        self.g_querykey += 1
+        output = EntityLinkPipeline_step1(
+            query_text=caption,
+            query_image=image,
+            multi_model=self.multi_model,
+            NER_model=self.NER_model,
+            multi_processor=self.flava_processor,
         )
-        entities[idx]["answer"] = query_results[idx][ans]["id"]
+        self.pipeline_cache[self.g_querykey] = output
+        query = [entity['entity'] for entity in output.entities]
+        return {"key": self.g_querykey, "query": query}
 
-    return entities
+    def MEL_step2(self, key: int, queries: list):
+        result = EntityLinkPipeline_step2(
+            preoutput=self.pipeline_cache[key],
+            entity_model=self.entity_model,
+            entity_processor=self.entity_processor,
+            wikidata=queries,
+        )
+        del self.pipeline_cache[key]
+        return result
+
+    def MEL_step2V2(self, key: int, query_results: list):
+        preoutput = self.pipeline_cache[key]
+        entities = preoutput.entities
+        del self.pipeline_cache[key]
+
+        for idx, q in enumerate(query_results):
+            ans = EntityLinkPipelineV2(
+                query_text=preoutput.query_text,
+                query_img=preoutput.query_image,
+                candidate_abs=[cand["abs"] for cand in q],
+                model=self.el_model,
+                processor=self.flava_processor,
+            )
+            entities[idx]["answer"] = query_results[idx][ans]["id"]
+
+        return entities
